258638663acb87d20f1fa1d84f6af0eba53e1c4c
coffeegoddd <dbeaubrown@gmail.com>
2018-12-19 13:31:07 -0800

    Added initial file structure, pseudocoded some starting points for data generation functions and for accompanying jest unit tests

---
+++ db/seed/generateData.js
@@ -0,0 +1,10 @@
+//function1 that returns an array of 1000 objects matching the Units table properties
+
+//function2 that returns an array of 1000 objects matching the Amenities table properties
+
+//function3 that returns an array of 1000 objects matching the Owners table properties
+
+//function4 that iterates over the 1000 unitIds and assigns each unit a random number of
+  //amenities. Return an array of objects that contain a unitId prop whos value is a unitId,
+  //and an ameities prop with an array containing a random number of amenityIds that will be
+  //associated with each unit...
---
+++ db/seed/generateData.test.js
@@ -0,0 +1,18 @@
+//function1
+
+//expect return value to be an array
+//expect length of array to be 1000
+
+//function2
+
+//expect return value to be an array
+//expect length of array to be 1000
+
+//function3
+
+//expect return value to be an array
+//expect length of array to be 1000
+
+//function4 
+
+//need to design useful test for this one...
