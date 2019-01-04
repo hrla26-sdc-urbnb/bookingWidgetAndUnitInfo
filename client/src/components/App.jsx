1c2115c96dffdf28b04c7f103509505953e9420f
coffeegoddd <dbeaubrown@gmail.com>
2019-01-04 06:55:38 -0800

    Switched to relative path for axios request

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -63,7 +63,7 @@ class App extends React.Component {
   }
 
   fetchUnit() {
-    axios.get(`http://localhost:2100/api/units/${generateRandomNumberBetween(11111, 11210)}`)
+    axios.get(`/api/units/${generateRandomNumberBetween(11111, 11210)}`)
       .then(({ data }) => {
         console.log('axios -->', data);
         const { unitData, ownerData } = data;
