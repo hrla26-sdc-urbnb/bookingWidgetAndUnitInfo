d8d0b2032de60c5e3a35a0583cd69d69f2ca3e69
coffeegoddd <dbeaubrown@gmail.com>
2019-01-05 12:31:13 -0800

    Added methods to ShowAmens that update state on mount

--- client/src/components/unitInfo/ShowAmens.jsx
+++ client/src/components/unitInfo/ShowAmens.jsx
@@ -12,7 +12,7 @@ class ShowAmens extends React.Component {
       notIncluded: [],
     };
 
-    this.map = {
+    this.mapAmenSentences = {
       hasAirConditioning: 'Air conditioning',
       hasBreakfastCoffeeTea: 'Breakfast',
       hasCarbonMonoxideDetector: 'Carbon monoxide detector',
@@ -39,25 +39,164 @@ class ShowAmens extends React.Component {
       hasTv: 'TV',
       hasWifi: 'Wifi',
     };
+    this.updateBasic = this.updateBasic.bind(this);
+    this.updateFacilities = this.updateFacilities.bind(this);
+    this.updateDining = this.updateDining.bind(this);
+    this.updateBedAndBath = this.updateBedAndBath.bind(this);
+    this.updateSafetyFeatures = this.updateSafetyFeatures.bind(this);
+    this.updateNotIncluded = this.updateNotIncluded.bind(this);
+  }
 
+  componentDidMount() {
     const { unitData } = this.props;
-    const hasAmens = [];
-    const missingAmens = [];
+    this.updateBasic(unitData);
+    this.updateFacilities(unitData);
+    this.updateDining(unitData);
+    this.updateBedAndBath(unitData);
+    this.updateSafetyFeatures(unitData);
+    this.updateNotIncluded(unitData);
+  }
 
-    for (let key in unitData) {
-      if (typeof unitData[key] === 'boolean' && key != 'isBooked') {
-       if (unitData[key]) {
-         hasAmens.push(key);
-       } else {
-         missingAmens.push(key);
-       }
+  updateBasic(data) {
+    const dataKeys = Object.keys(data);
+    const basic = [];
+    for (let i = 0; i < dataKeys.length; i += 1) {
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasWifi') {
+        basic.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasEssentials') {
+        basic.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasTv') {
+        basic.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasDeskWorkSpace') {
+        basic.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasIron') {
+        basic.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasLaundryWasher') {
+        basic.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasLaundryDryer') {
+        basic.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasAirConditioning') {
+        basic.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasHeat') {
+        basic.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasClosetDrawers') {
+        basic.push(this.mapAmenSentences[dataKeys[i]]);
       }
     }
-    
+    this.setState({
+      basic,
+    });
+  }
 
+  updateFacilities(data) {
+    const dataKeys = Object.keys(data);
+    const facilities = [];
+    for (let i = 0; i < dataKeys.length; i += 1) {
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasParking') {
+        facilities.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasPool') {
+        facilities.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasHotTub') {
+        facilities.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasElevator') {
+        facilities.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasPrivateEntrance') {
+        facilities.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasFirePlace') {
+        facilities.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+    }
+    this.setState({
+      facilities,
+    });
+  }
+  
+  updateDining(data) {
+    const dataKeys = Object.keys(data);
+    const dining = [];
+    for (let i = 0; i < dataKeys.length; i += 1) {
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasParking') {
+        dining.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasBreakfastCoffeeTea') {
+        dining.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+    }
+    this.setState({
+      dining,
+    });
+  }
+
+  updateBedAndBath(data) {
+    const dataKeys = Object.keys(data);
+    const bedAndBath = [];
+    for (let i = 0; i < dataKeys.length; i += 1) {
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasHairDryer') {
+        bedAndBath.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasShampoo') {
+        bedAndBath.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+    }
+    this.setState({
+      bedAndBath,
+    });
+  }
+
+  updateSafetyFeatures(data) {
+    const dataKeys = Object.keys(data);
+    const safetyFeatures = [];
+    for (let i = 0; i < dataKeys.length; i += 1) {
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasCarbonMonoxideDetector') {
+        safetyFeatures.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasFireExtinguisher') {
+        safetyFeatures.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasSmokeDetector') {
+        safetyFeatures.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasFirstAidKit') {
+        safetyFeatures.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+      if (data[dataKeys[i]] && dataKeys[i] === 'hasLockOnBedroomDoor') {
+        safetyFeatures.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+    }
+    this.setState({
+      safetyFeatures,
+    });
+  }
+
+  updateNotIncluded(data) {
+    const dataKeys = Object.keys(data);
+    const notIncluded = [];
+    for (let i = 0; i < dataKeys.length; i += 1) {
+      if (!data[dataKeys[i]] && dataKeys[i] !== 'isBooked') {
+        notIncluded.push(this.mapAmenSentences[dataKeys[i]]);
+      }
+    }
+    this.setState({
+      notIncluded,
+    });
   }
 
   render() {
+    console.log(this.state);
     return (
       <div>
         this is the amens all...
