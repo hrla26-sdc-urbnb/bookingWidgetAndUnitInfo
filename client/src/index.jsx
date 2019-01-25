8e0389a00f38b1a339e200fc820e2a871ee236e6
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-01-24 21:11:44 -0800

    Install babel dependencies, start on server-side rendering

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,8 +6,8 @@ import BookingWidgetIso from './components/BookingWidgetIso.jsx';
 const generateRandomNumberBetween = (beg, end) => Math.floor((Math.random() * (end - beg + 1)) + beg);
 const id = generateRandomNumberBetween(4569598, 14569595); // min and max ids in units db table
 
-// ReactDOM.hydrate...
-// ReactDOM.hydrate...
-
 ReactDOM.render(<App id={id} />, document.getElementById('descript'));
 ReactDOM.render(<BookingWidgetIso id={id} />, document.getElementById('_bookingWidget'));
+
+// ReactDOM.hydrate(<App id={id} />, document.getElementById('descript'));
+// ReactDOM.hydrate(<BookingWidgetIso id={id} />, document.getElementById('_bookingWidget'));
